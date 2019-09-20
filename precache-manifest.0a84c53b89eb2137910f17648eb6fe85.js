9a2a65036b4b22097f5d8993fa0074f275fe9ad8
wannab3d3v3lop3r <wannab3d3v3lop3r@gmail.com>
2019-09-19 22:15:11 -0400

    Updates

---
+++ precache-manifest.0a84c53b89eb2137910f17648eb6fe85.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cb46f43f88d8567aa480cf618f73e49",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0ef14519989ea93ea2f4",
+    "url": "/static/css/main.275bd464.chunk.css"
+  },
+  {
+    "revision": "40a73f52dea45a706e42",
+    "url": "/static/js/2.7173a56b.chunk.js"
+  },
+  {
+    "revision": "0ef14519989ea93ea2f4",
+    "url": "/static/js/main.7bc2fd3a.chunk.js"
+  },
+  {
+    "revision": "cb111ddbbbbe49bf5266",
+    "url": "/static/js/runtime-main.30e21a9a.js"
+  }
+]);
